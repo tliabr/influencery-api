54b19af2a7ec47993f491033801ada1d22703e99
Thallia <thalliaborbarosa@gmail.com>
2022-03-04 19:18:48 +0000

    fixed primary tag association

--- app/models/influencer.rb
+++ app/models/influencer.rb
@@ -1,5 +1,6 @@
 class Influencer < ApplicationRecord
   belongs_to :platform
+  belongs_to :primary_tag, class_name: "Tag"
   has_many :influencer_tags
   has_many :tags, :through => :influencer_tags
 end
--- app/serializers/influencer_serializer.rb
+++ app/serializers/influencer_serializer.rb
@@ -1,4 +1,3 @@
 class InfluencerSerializer < ActiveModel::Serializer
-  # added :primary_tag_id
-  attributes :id, :handle, :platform, :followers, :profile_pic_url, :tags, :primary_tag_id
+  attributes :id, :handle, :platform, :followers, :profile_pic_url, :tags, :primary_tag
 end
